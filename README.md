2182e18edf33ffad58ecd072d56fefa91139bce6
Muhammadfahim17 <fahimbodurov@gmail.com>
2025-09-02 20:28:28 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Practice-Lesson-or-Type-Script
---
+++ script.js
@@ -0,0 +1,40 @@
+// let userName = "Sobir"
+// console.log(userName);
+// let h1 = document.querySelector(".h1") as HTMLElement
+// console.log((h1.innerHTML = "Sobir"));
+// let a = 999999999999999 
+// console.log(a);
+// let userName:string = "Sobir"
+// let greeting:string = "Hello My Friend"
+// let message:string = `Soobshenia dlya ${userName} : ${greeting}`
+// console.log(message);
+// let age: number = 25
+// let temparature: number = -5.7
+// let distance: number = 1e5
+// console.log(distance);
+// console.log(age);
+// console.log(temparature);
+// let isActive: boolean = true
+// let isInactive: boolean = false
+// let hasPermesson: boolean = (10 > 5)
+// console.log(isActive);
+// console.log(isInactive);
+// console.log(hasPermesson);
+// let undefinedVar: undefined = undefined
+// let nullVar: null = null
+// let emptyValue: string | null = null
+// console.log(emptyValue);
+// let flexibleVar: any = "Sobir"
+// flexibleVar = 8
+// flexibleVar = true
+// console.log(flexibleVar);
+// let uncertainValue: unknown = "In Sobir ast"
+// uncertainValue = 55
+// if (typeof uncertainValue === 'number'){
+//     console.log(uncertainValue * 55);
+// }
+// console.log(uncertainValue);
+// function userInfo(name : string, age : number) : void {
+//     console.log(`Nomi in shaxs ${name} ast , vay ${age} sola ast`)
+// }
+// userInfo("Sobir",55)
